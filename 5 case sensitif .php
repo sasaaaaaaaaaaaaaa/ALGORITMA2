9d5df315364ed92aae891f9bbc27d52832e07154
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-27 13:34:42 +0700

    membuat case sensitif

---
+++ 5 case sensitif .php
@@ -0,0 +1,24 @@
+<?php
+
+$nama = "alika naswa sabila";
+$NAMAKAWANSAYA= "putri nayla";
+$umur =16tahun;
+const INI_KOSTANTA =123;
+$ini_variabel = 23;
+$iniJugaVariabel = 49;
+$iniJugaVariabel;
+$tanggallahir = 23;
+
+echo " ==== mencetak variabel ==== ";
+echo "<p>Nama saya adalah $nama";
+echo "<p>Nama ketua kelas adalah $NAMAKAWANSAYA";
+echo "<p>Umur saya adalah $umur";
+
+echo 
+
+    "<p>coba pakai blok program",
+    "<p>Nama saya adalah $nama",
+    "<p>Nama ketua kelas adalah $NAMAKAWANSAYA",
+    "<p>Umur saya adalah $umur";
+
+?>
