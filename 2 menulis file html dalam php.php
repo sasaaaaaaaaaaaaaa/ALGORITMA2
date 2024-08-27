7b3ce9b178d90eae5af4dee4072cb3b5c937a223
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-27 11:24:01 +0700

    komit pertama

---
+++ 2 menulis file html dalam php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "kita sedang belajar PHP<br>";
+            echo "<p>Belajar PHP hingga jadi master</p>";
+        ?>
+    </body>
+</html>
