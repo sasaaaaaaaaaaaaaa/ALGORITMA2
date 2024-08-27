0ad2a88cb7f46ff236f6969bc01a772596bde5ac
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-27 12:36:04 +0700

    membuat statemen dan ekspresi

---
+++ 4 menulis statemen dan ekspresi.php
@@ -0,0 +1,16 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+// deklarasi sebuah variable
+$b=23;
+$c=7;
+// ekspesi
+$a1 = $b * $c;
+$a2 = $b + $c;
+//cetak ekspresi
+echo "<p> ini expresi -> $a1= $b * $c <p>" ;
+echo "<p> ini expresi -> $a2 = $b + $c <p>";
+echo "<P> yaitu :".$a1;
+echo" <p> yaitu :".$a2;
+echo " <p> by : alika nasa sabila"
+$a = $b + $c;
