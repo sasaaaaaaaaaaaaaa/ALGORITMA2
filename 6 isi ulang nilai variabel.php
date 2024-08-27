89db99ad26dafeb1729f9c819f72b68dcb1d8525
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-27 14:05:52 +0700

    membuat ulangan nilai variabel

---
+++ 6 isi ulang nilai variabel.php
@@ -0,0 +1,15 @@
+<?php 
+
+// ini adalah komentar
+echo "Hello world";
+
+/*
+ini adalah komentar 
+yang lebih dari satu 
+baris
+*/
+echo "saya sudah dewasa";
+echo "<br> saya suka makan buah";
+echo "<br> by : alika naswa sabila";
+
+?>
