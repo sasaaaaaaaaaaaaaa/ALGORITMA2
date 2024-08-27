cd5c49a8b627ba0d869c3ed6649e25af62ec5fbd
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-27 22:19:58 +0700

    menampilkan nilai dari variabel

---
+++ 9 mencetak dengan echo.php
@@ -0,0 +1,8 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "bunga matahari";
+$harga = 150000;
+$tempat = "toko bunga";
+// menampilkan isi variabel
+echo "sasa membeli $nama_barang seharga Rp $harga di$tempat ";
