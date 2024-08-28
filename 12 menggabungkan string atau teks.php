ada95faf2264fe4160f7ce3e53dc266f0fe4f0d0
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-28 08:18:59 +0700

    menggabungkan string di php.php

---
+++ 12 menggabungkan string atau teks.php
@@ -0,0 +1,11 @@
+<?php
+$txt1 = "Tutorial PHP";
+$txt2 = "alika naswa sabila";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Dibelajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
