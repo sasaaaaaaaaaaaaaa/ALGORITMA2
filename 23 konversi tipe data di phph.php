32bd03a14f8bb776eb8d8ded2f5bc2c56852144b
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-28 09:29:08 +0700

    membuat konversi tipe data

---
+++ 23 konversi tipe data di phph.php
@@ -0,0 +1,7 @@
+
+$foo = "1";  // mula-mual $foo dalam bentuk string (ASCII 49)
+$foo *= 2;   // $foo sekarang adalah integer (2)
+$foo = $foo * 1.3;  // $foo sekarang adalah float (2.6)
+$foo = 5 *"10 Little birds"; // $foo sekarang adalah integer (50)
+$foo = 5 *"10 Small birds"; // $foo sekarang adalah integer (50)
+?>
