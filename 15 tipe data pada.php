3b990e666cd2cf1479cff4ef1ae8d36bce9c7dbf
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-28 08:43:44 +0700

    membuat tipe data php

---
+++ 15 tipe data pada.php
@@ -0,0 +1,27 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "ALIKA NASWA SABILA";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 50;
+
+// tipe data float
+$tinggi = 152;
+
+// tipe data boolean
+$pelajar = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "pelajar:$pelajar";
+?>
